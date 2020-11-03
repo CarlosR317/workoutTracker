2b9d75ffd65791d428a4eb1dd5d501f749a0c96e
CarlosR317 <cramire5@ycp.edu>
2020-11-03 13:27:11 -0500

    more fixed on connections

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -3,21 +3,21 @@ const path = require("path");
 module.exports = (app) => {
 
 
-    app.get("/index", (req, res) => {
+    app.get("/", (req, res) => {
 
-        res.sendFile(path.join(__dirname, "./public/index.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
 
     });
 
     app.get("/exercise", (req, res) => {
 
-        res.sendFile(path.join(__dirname, "./public/exercise.html"));
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
 
     });
 
     app.get("/stats", (req, res) => {
 
-        res.sendFile(path.join(__dirname, "./public/stats.html"));
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
 
     });
 
